c3239a1f49cf3891c9b97aa5c8f760bca94db40f
mdsrmed <mdsrmed@gmail.com>
2022-08-24 19:39:25 -0400

    fetch comment and render

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -5,17 +5,36 @@ import Sidebar from "../../components/Sidebar";
 import Widgets from "../../components/Widgets";
 import Post from "../../components/Post";
 import { useRouter } from "next/router";
-import { onSnapshot } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  documentId,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
+import { db } from "../../firebase";
+import Comment from "../../components/comment";
 
 export default function PostPage({ newsResults, randomUserResults }) {
   const router = useRouter();
   const { id } = router.query;
   const [post, setPost] = useState();
+  const [comments, setComments] = useState([]);
   useEffect(
     () => onSnapshot(doc(db, "posts", id), (snapshot) => setPost(snapshot)),
     [db, id]
   );
+  useEffect(() => {
+    onSnapshot(
+      query(
+        collection(db, "posts", id, "comments"),
+        orderBy("timestamp", "desc")
+      ),
+      (snapshot) => setComments(snapshot.docs)
+    );
+  }, [db, id]);
   return (
     <div>
       <Head>
@@ -29,19 +48,30 @@ export default function PostPage({ newsResults, randomUserResults }) {
         <Sidebar />
 
         {/* feed */}
-        <div>
-          <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[580px] sm:ml-[73px] flex-grow max-w-xl">
-            <div className="flex items-center space-x-2 justify-between py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
-              <div className="hoverEffect" onClick={() => router.push("/")}>
-                <ArrowLeftIcon className="h-5" />
-              </div>
-              <h2 className="text-lg sm:text-xl font-bold cursor-pointer p-1">
-                Tweet
-              </h2>
+
+        <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[580px] sm:ml-[73px] flex-grow max-w-xl">
+          <div className="flex items-center space-x-2 justify-between py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
+            <div className="hoverEffect" onClick={() => router.push("/")}>
+              <ArrowLeftIcon className="h-5" />
             </div>
-            <Post id={id} post={post} />
+            <h2 className="text-lg sm:text-xl font-bold cursor-pointer p-1">
+              Tweet
+            </h2>
           </div>
+          <Post id={id} post={post} />
+          {comments.length > 0 && (
+            <div>
+              {comments.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  id={comment.id}
+                  comment={comment.data()}
+                />
+              ))}
+            </div>
+          )}
         </div>
+
         {/* widgets */}
         <Widgets
           newsResults={newsResults.articles}
